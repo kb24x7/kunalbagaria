5223f2cc165616fb68caa5ffad1efff38d4510c2
Kunal Bagaria <kb24x7@gmail.com>
2020-12-09 16:23:51 +0530

    fix

--- space/main.js
+++ space/main.js
@@ -1,5 +1,27 @@
 let aud = document.getElementById('bg-audio')
+
 function play() {
     // @ts-ignore
     aud.play()
 }
+const fetchSpaceNews = async () => {
+    try {
+        // @ts-ignore
+        const response = await fetch('https://spaceflightnewsapi.net/api/v2/articles')
+        const dataNews = await response.json()
+        return dataNews
+    } catch (error) {
+        console.log(error)
+    }
+}
+async function loadPage() {
+    let dataNews
+    dataNews = await fetchSpaceNews()
+    let latestNews = dataNews[0]
+    let secondLatest = dataNews[1]
+    let thirdLatest = dataNews[2]
+    let fourthLatest = dataNews[3]
+    let fifthLatest = dataNews[4]
+    console.log(latestNews)
+}
+loadPage()
