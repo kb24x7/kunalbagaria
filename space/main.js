0f56ca1f145a7c032217f77c95ba0fd470de4d5b
Kunal Bagaria <kb24x7@gmail.com>
2020-12-09 18:39:51 +0530

    day21

--- space/main.js
+++ space/main.js
@@ -1,8 +1,19 @@
 let aud = document.getElementById('bg-audio')
+let title = document.getElementById('title')
+let summary = document.getElementById('summary')
+let imageUrl = document.getElementById('imageUrl')
+let newsLink = document.getElementById('newsLink')
+let disPause = document.getElementById('pause')
+let disPlay = document.getElementById('play')
 
 function play() {
     // @ts-ignore
     aud.play()
+}
+
+function pause() {
+    // @ts-ignore
+    aud.pause()
 }
 const fetchSpaceNews = async () => {
     try {
@@ -14,14 +25,37 @@ const fetchSpaceNews = async () => {
         console.log(error)
     }
 }
-async function loadPage() {
-    let dataNews
-    dataNews = await fetchSpaceNews()
-    let latestNews = dataNews[0]
-    let secondLatest = dataNews[1]
-    let thirdLatest = dataNews[2]
-    let fourthLatest = dataNews[3]
-    let fifthLatest = dataNews[4]
-    console.log(latestNews)
-}
+
+let dataNews
+let newsNumber = 0;
 loadPage()
+
+function next() {
+    let totalLength = dataNews.length - 1
+    if (newsNumber < totalLength) {
+    newsNumber++
+    addNews()
+    }
+}
+function prev() {
+    if (newsNumber != 0) {
+        newsNumber--
+    }
+    addNews()
+}
+
+async function loadPage() {
+    dataNews = await fetchSpaceNews()
+    console.log(newsNumber)
+    addNews()
+}
+
+function addNews() {
+    console.log(dataNews[newsNumber])
+    // @ts-ignore
+    newsLink.href = dataNews[newsNumber].url
+    title.innerHTML = dataNews[newsNumber].title
+    summary.innerHTML = dataNews[newsNumber].summary
+    // @ts-ignore
+    imageUrl.src = dataNews[newsNumber].imageUrl
+}
